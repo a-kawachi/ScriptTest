c5e76f1a850057c2d66bdfa85c1d0775d7636154
a-kawachi <kawachi1420@gmail.com>
2021-07-19 16:34:45 +0900

    a
    a

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -9,29 +9,35 @@ public class Test : MonoBehaviour
     void Start()
     {
         // 要素数5の配列を初期化する
-        int[] array = new int[4];
+        int[] array = new int[5];
 
         // 配列の各要素に値を代入する
         array[0] = 30;
         array[1] = 20;
         array[2] = 50;
         array[3] = 10;
+        array[4] = 60;
 
         // 配列の要素をすべて表示する
         for (int i = 0; i < 4; i++)
         {
             Debug.Log(array[i]);
         }
-    }
 
-    void Start()
-    {
+
         for (int i = 0; i < 5; i++)
         {
             Debug.Log(i);
         }
-    }
 
+
+
+
+        for (int i = 4; i >= 0; i--)
+        {
+            Debug.Log(i);
+        }
+    }
 
     // Update is called once per frame
     void Update()
