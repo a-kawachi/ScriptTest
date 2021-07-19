f3d594041f557557d75beb3a288c2b4ba7bbd80f
a-kawachi <kawachi1420@gmail.com>
2021-07-19 10:56:14 +0900

    Initial commit

---
+++ Assets/Test.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        // 要素数5の配列を初期化する
+        int[] array = new int[4];
+
+        // 配列の各要素に値を代入する
+        array[0] = 30;
+        array[1] = 20;
+        array[2] = 50;
+        array[3] = 10;
+
+        // 配列の要素をすべて表示する
+        for (int i = 0; i < 4; i++)
+        {
+            Debug.Log(array[i]);
+        }
+    }
+
+    void Start()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Debug.Log(i);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
